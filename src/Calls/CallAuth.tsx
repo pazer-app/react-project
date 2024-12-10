9d76ffd27642db58f51199e81f67c4597f549624
davidkim <woody@woody.co.kr>
2024-12-10 22:30:11 +0900

    Add Files

--- src/Calls/CallAuth.tsx
+++ src/Calls/CallAuth.tsx
@@ -2,6 +2,10 @@ import {Calls} from "./Calls"
 
 export function CallLogin() : void {
     const req = new Calls()
-    req.method("POST").endpoint("test").addHeader("Auth-Token","test").addData("test","test")
+    req.method("GET")
+        .endpoint("test")
+        .addHeader("Auth-Token","sdf123")
+        .addHeader("Auth-Sid","kds")
+        .addData("test","test")
     req.send().then(() => {})
 }
